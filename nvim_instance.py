4b1afcb6ae89419c0fd37270638d6b0caac67a1b
xchrix <chrix_liu@qq.com>
2023-08-22 11:18:49 +0800

    format src file

--- nvim_instance.py
+++ nvim_instance.py
@@ -19,14 +19,18 @@ if __name__ == '__main__':
     elif sys.platform == 'linux':
         PIPE_DIR = r"/tmp/nvim/"
 
-    parser = argparse.ArgumentParser(description="Nvim Instance management, launch one unique Nvim GUI for one path")
+    parser = argparse.ArgumentParser(
+        description="Nvim Instance management, launch one unique Nvim GUI for one path")
     # parser.add_argument('--exec', '-e', help='Nvim gui command, use neovide by default', default="neovide")
     parser.add_argument('--project', '-p',
                         help='project path, open same path will reuse previous window, necessary argument',
                         required=True)
-    parser.add_argument('--file', '-f', help='file name, necessary argument', required=True)
-    parser.add_argument('--line', '-l', help='line number to jump to', default=0)
-    parser.add_argument('--column', '-c', help='column number to jump to', default=0)
+    parser.add_argument(
+        '--file', '-f', help='file name, necessary argument', required=True)
+    parser.add_argument(
+        '--line', '-l', help='line number to jump to', default=0)
+    parser.add_argument(
+        '--column', '-c', help='column number to jump to', default=0)
     args = parser.parse_args()
 
     # command = args.exec
@@ -73,7 +77,7 @@ if __name__ == '__main__':
             print("retry too many times, fail")
             quit(-1)
 
-        if i ==0:
+        if i == 0:
             # ret: int = os.system(command + " -- --listen " + PIPE_NAME)
             ret: int = os.system("wt nt nvim --listen " + PIPE_NAME + " ")
             # ret: int = os.system("neovide -- --listen " + PIPE_NAME)
@@ -83,4 +87,3 @@ if __name__ == '__main__':
                 print("create a new nvim instance")
             time.sleep(0.1)
         i += 1
-
